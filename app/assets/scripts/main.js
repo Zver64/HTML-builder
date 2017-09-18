49d1c69b5dab851a7bcf6bc7725333d42b965539
Zver64 <coolvill@mail.ru>
2017-09-18 17:56:53 +0300

    first commit

---
+++ app/assets/scripts/main.js
@@ -0,0 +1,4 @@
+//=../../bower_components/jquery/dist/jquery.js
+//=../../jquery.form.js
+//=../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.js
+//=../../bower_components/fancybox/dist/jquery.fancybox.js
